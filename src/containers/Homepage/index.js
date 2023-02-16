fe6fd9075857ea6ce7aafc676f3de4863830bd81
Venugopal <venugopala@arzooo.com>
2023-02-16 13:18:58 +0530

    Homepage - Full homepage with images and content.

--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -1,9 +1,105 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Container from "../../components/Container";
+import Layout from "../../components/Layout";
+
+import { exploreSite, keyFeatures } from "../../utils/constants/site-settings";
+
+import HeroSection from "../../assets/images/homepage/hero-section.svg";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <Layout>
+      <Container>
+        <div className="grid grid-cols-12 py-20">
+          <div className="col-span-12 md:col-span-6 flex flex-col justify-center text-4xl">
+            <span className="font-semibold">
+              Are you tired of dealing with multiple companies for your real
+              estate needs?
+            </span>
+            <br />
+            <p className="">
+              Look no further than{" "}
+              <span className="text-primary font-bold">Realyfy!</span>
+            </p>
+          </div>
+          <div className="col-span-12 md:col-span-6 flex items-center justify-center">
+            <img src={HeroSection} alt="hero-section" />
+          </div>
+        </div>
 
-export default Homepage
+        <div className="flex flex-col items-center py-20">
+          <h1 className="text-primary text-2xl font-extrabold mb-1 text-center">
+            FEATURES
+          </h1>
+          <h2 className="text-gray-600 text-5xl font-semibold mb-6 text-center">
+            We have Amazing Features.
+          </h2>
+          <p className="text-gray-500 text-lg font-semibold text-center max-w-4xl">
+            At Realyfy, we offer a wide range of services to help you with all
+            your needs. Whether you&#39;re looking for a house to buy or sell,
+            home loan, interior design services, or home maintenance, we&#39;ve
+            got you covered. With our one-stop-shop approach, we make the real
+            estate process simple and stress-free.
+          </p>
+
+          <div className="w-full grid grid-cols-3 my-20 gap-10">
+            {keyFeatures?.map((feature) => (
+              <div className="grid grid-cols-12">
+                <div className="col-span-4 text-4xl flex justify-center">
+                  <div className="w-20 h-20 flex items-center justify-center border border-gray-300 rounded-full text-primary">
+                    {feature.icon}
+                  </div>
+                </div>
+                <div className="col-span-8">
+                  <h2 className="text-2xl font-bold mb-2">{feature.heading}</h2>
+                  <p className="text-1xl text-gray-500 font-semibold">
+                    {feature.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex flex-col items-center py-20">
+            <h1 className="text-primary text-2xl font-extrabold mb-1 text-center">
+              Explore
+            </h1>
+
+            <h2 className="text-gray-600 text-5xl font-semibold mb-6 text-center">
+              Our wide range of services
+            </h2>
+
+            <p className="text-gray-500 text-lg font-semibold text-center mb-8 max-w-4xl">
+              At Realyfy, we offer a wide range of services to help you with all
+              your needs. Whether you&#39;re looking for a house to buy or sell,
+              home loan, interior design services, or home maintenance,
+              we&#39;ve got you covered. With our one-stop-shop approach, we
+              make the real estate process simple and stress-free.
+            </p>
+
+            <div className="grid grid-cols-4 gap-3">
+              {exploreSite?.map((explore) => (
+                <Link to={explore.link}>
+                  <div>
+                    <img
+                      src={explore.image}
+                      alt="real-estate"
+                      className="w-full h-96 bg-gray-300"
+                    />
+                    <div className="bg-gray-500 text-xl text-white text-center py-2">
+                      {explore.label}
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Homepage;
---
+++ src/utils/constants/site-settings.js
@@ -0,0 +1,69 @@
+import RealEstate from "../../assets/images/explore/real-estate.jpg";
+import Interiros from "../../assets/images/explore/interiors.jpg";
+import HomeServices from "../../assets/images/explore/home-services.jpg";
+import Finance from "../../assets/images/explore/finance.jpg";
+
+export const keyFeatures = [
+  {
+    heading: "Experienced agents",
+    description:
+      "realyfy has a team of experienced agents who can provide personalized attention and guidance to clients throughout the buying or selling process.",
+    icon: <i class="fa-solid fa-user"></i>,
+  },
+  {
+    // heading: "Competitive rates",
+    // description:
+    //   "realyfy is committed to offering competitive rates and value to clients for all of their real estate needs.",
+    heading: "0% brokerage",
+    description:
+      "realyfy is commited to offer you 0% brokerage on your proerty purchase.",
+    icon: <i class="fa-solid fa-percent"></i>,
+  },
+  {
+    heading: "Quality properties",
+    description:
+      "realyfy offers quality properties for sale or rent that are in compliance with local laws and regulations.",
+    icon: <i class="fa-solid fa-house"></i>,
+  },
+  {
+    heading: "Interior design",
+    description:
+      "realyfy offers interior design services to help clients prepare their properties for sale.",
+    icon: <i class="fa-solid fa-couch"></i>,
+  },
+  {
+    heading: "Trusted lenders",
+    description:
+      "realyfy has partnerships with trusted lenders who can assist clients in securing home loans or mortgages.",
+    icon: <i class="fa-solid fa-handshake"></i>,
+  },
+  {
+    heading: "Exceptional customer service",
+    description:
+      "realyfy is dedicated to providing exceptional customer service to all clients, with prompt communication and attention to their individual needs.",
+    icon: <i class="fa-solid fa-headset"></i>,
+  },
+];
+
+export const exploreSite = [
+  {
+    link: "/real-estate",
+    image: RealEstate,
+    label: "Real Estate",
+  },
+  {
+    link: "/interiors",
+    image: Interiros,
+    label: "Interiors",
+  },
+  {
+    link: "/home-services",
+    image: HomeServices,
+    label: "Home Services",
+  },
+  {
+    link: "/finance",
+    image: Finance,
+    label: "Finance",
+  },
+];
