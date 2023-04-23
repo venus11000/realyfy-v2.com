214c4ba788f6c0a550259edc8671ee0e85b8a657
Venugopal <venugopala11000@gmail.com>
2023-04-24 00:57:44 +0530

    Dashboard - prtecting

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -3,13 +3,24 @@ import React from "react";
 import Container from "../../components/Container";
 import Layout from "../../components/Layout";
 import LoginForm from "./LoginForm";
+import { LOCAL_STORAGE_KEYS } from "../../utils/constants/site-settings";
+import { useNavigate } from "react-router-dom";
 
-const ContactUs = () => {
+const Dashboard = () => {
+  const navigate = useNavigate()
+  const authToken = localStorage.getItem(LOCAL_STORAGE_KEYS.AUTH_TOKEN);
+
+  if (!authToken) {
+    navigate("/");
+  };
+
   return (
     <Layout>
       <Container className="my-32 flex flex-col items-center justify-center">
         {/* <div className="max-w-lg w-full border border-primary rounded-2xl p-10"> */}
-        <h1 className="text-center text-3xl text-primary font-semibold mb-10">Welcome</h1>
+        <h1 className="text-center text-3xl text-primary font-semibold mb-10">
+          Welcome
+        </h1>
         <LoginForm />
         {/* </div> */}
       </Container>
@@ -17,4 +28,4 @@ const ContactUs = () => {
   );
 };
 
-export default ContactUs;
+export default Dashboard;
